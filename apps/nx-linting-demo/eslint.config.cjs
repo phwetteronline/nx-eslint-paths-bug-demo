be38ab0201d6ce49f26125d42f4c4834de9f35f6
Paul Heising <paul.heising@wetteronline.de>
2025-01-27 23:13:14 +0100

    eslint config with explainers

--- apps/nx-linting-demo/eslint.config.cjs
+++ apps/nx-linting-demo/eslint.config.cjs
@@ -24,6 +24,30 @@ module.exports = [
           style: 'kebab-case',
         },
       ],
+      // example default behaviour for all TS files in this project:
+      'no-console': 'error',
+    },
+  },
+  {
+    // Say we want to change the severity of no-console for a given file. We can
+    // do this by providing the file path here.
+    // Path relative to workspace root: Works as expected when running `nx lint
+    // nx-linting-demo`, but pattern does not match the main.ts when the VSCode
+    // extension lints it.
+    files: ['apps/nx-linting-demo/src/main.ts'],
+    //-----
+    // If you comment the `files` property above and uncomment the one below,
+    // you will see a change in behaviour. When running `nx lint
+    // nx-linting-demo`, you will get errors reported the console.logs in
+    // main.ts, rather than warnings. But when using the ESLint extension for
+    // VSCode, you will see warnings as expected. Also, when you go to the
+    // project root and run `npx eslint src/main.ts`, it will output the logs as
+    // warnings, as expected. It seems like the VSCode extension runs eslint
+    // from the directory where the config is, and I think that is the most
+    // convenient way to provide the pathes: relative to the config file.
+    // files: ['src/main.ts'],
+    rules: {
+      'no-console': 'warn',
     },
   },
   {
